eefaefea505a2024eb62944e39c5274283892f98
loewenm <michael.loewen@gmail.com>
2020-02-09 16:31:06 -0500

    full copy/paste from Pumperla's repo as book instructions are invalid

--- code/dlgo/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  7 17:38:16 2020
-
-@author: micha
-"""
-
--- code/dlgo/agent/__init__.py
+++ code/dlgo/agent/__init__.py
@@ -1,2 +1,7 @@
-# -*- coding: utf-8 -*-
-
+from .alphago import *
+from .base import *
+from .pg import *
+from .predict import *
+from .naive import *
+from .naive_fast import *
+from .termination import *
